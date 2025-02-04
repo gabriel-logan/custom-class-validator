2b9558657589782f9ef3e1a2426d758a12d065c2
Gabriel Logan <gabrielcoutolima@protonmail.ch>
2025-02-04 16:16:49 -0300

    fix: add clarification comment regarding validation limitations in CreateUserDto

--- src/users/dto/create-user.dto.ts
+++ src/users/dto/create-user.dto.ts
@@ -4,6 +4,9 @@ import { IsNotBlank } from "src/utils/decorators/IsNotBlank";
 import { IsNotEmpty } from "src/utils/decorators/IsNotEmpty";
 import { IsString } from "src/utils/decorators/IsString";
 
+// If any decorator is used like in `thisIsNotValidated` field, it will not be validated and will not be required in @Body().
+// This is a limitation of javascript runtime, because types do not exist in runtime.
+
 export class CreateUserDto {
   @ApiProperty()
   @IsString()
@@ -20,4 +23,6 @@ export class CreateUserDto {
   @ApiProperty()
   @IsString()
   password: string;
+
+  thisIsNotValidated: string;
 }
